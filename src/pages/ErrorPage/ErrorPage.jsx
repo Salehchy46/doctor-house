9cfae836bba840d5cb07514c8c6d96826b967747
salehchy46 <salehchyctg@gmail.com>
2025-09-06 12:12:08 +0600

    error element is added

---
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const ErrorPage = () => {
+    return (
+        <div
+            className="hero min-h-screen"
+            style={{
+                backgroundImage:
+                    "url(https://www.elegantthemes.com/blog/wp-content/uploads/2020/08/000-http-error-codes.png)",
+            }}
+        >
+            <div className="hero-overlay"></div>
+            <div className="hero-content text-center">
+                <div className="max-w-md">
+                    <h2 className="text-4xl text-center font-bold mb-56">The page is not found</h2>
+                    <Link to='/' className=''><button className="btn btn-primary mt-44">Home</button></Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ErrorPage;
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,9 +1,18 @@
 import { createBrowserRouter } from 'react-router-dom'
 import Main from '../layout/Main'
+import Home from '../pages/Home/Home'
+import ErrorPage from '../pages/ErrorPage/ErrorPage'
 
 export const router = createBrowserRouter([
     {
         path: '/',
-        element: <Main></Main>
+        element: <Main></Main>,
+        errorElement: <ErrorPage></ErrorPage>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>,
+            }
+        ]
     }
 ])
